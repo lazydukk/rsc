a98692744ba9311c74bc7f68b6fc30ac2795e6df
lazydukk <banul07@proton.me>
2023-03-06 04:53:29 +0530

    updated .elsintrc and .bashrc and added custom colors to tailwindcss config

---
+++ src/components/nav.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+   <nav className='flex items-center justify-betwee
+   flex-warp bg-transparent p-6'
+  );
+};
+
+export default Navbar;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'sessy-navy': '#001D3D',
+      'sessy-electro': '#00A8E8',
+      'sessy-navy-light': '#003566',
+      transparent: 'transparent',
+      current: 'currentColor'
+    }
   },
+  
   plugins: [],
 };
