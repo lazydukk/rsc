c1901ba303ca8abb12e0e32abff49d6eee78894f
lazydukk <bytex07@proton.me>
2023-06-13 00:08:17 +0530

    updated layout.tsx -- removed <br>

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Richmond Scrabble</h1>
-        <br>
         <h1>SHOWDOWN</h1>
         <h2>2023</h2>
       </div>
