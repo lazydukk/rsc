dfdc0b22c4cdb483231275e0a3562452c21540f5
lazydukk <bytex07@proton.me>
2023-06-13 00:07:04 +0530

    updated layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Richmond Scrabble</h1>
         <br>
         <h1>SHOWDOWN</h1>
-        </br>
         <h2>2023</h2>
       </div>
     </main>
+    </>
   )
 }
