a4622f0ed7a7655ceb6d8a176bde4e79009aacdf
lazydukk <banul07@proton.me>
2024-04-16 16:58:30 +0530

    nextjs & shadcn ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="text-xl tex-black bg-background">
+      <h1>hello, world</h1>
+    </main>
+  );
+}
